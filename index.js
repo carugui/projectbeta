a9cebcde096a6215a1105e87340eb08bf5ce928f
Carla C <carla.cc.cg@hotmail.com>
2023-03-22 12:51:16 +0100

    Modificado topbar con js para menu hamburguesa

--- index.js
+++ index.js
@@ -17,3 +17,32 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+
+
+
+const menu = document.querySelector(".nav__left");
+const menuItems = document.querySelectorAll(".nav__left__item");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".hamburger__icon--close");
+const menuIcon = document.querySelector(".hamburger__icon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
+
